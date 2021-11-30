fcbb089e5a049f9c9f173ac2422ab43d8e4b2de7
Sungjin <93691850+sungjinWi@users.noreply.github.com>
2021-11-30 20:46:10 +0900

    alert

--- js/signup.js
+++ js/signup.js
@@ -6,44 +6,48 @@ const email = document.querySelector("#email");
 const signUpBtn = document.querySelector("#signUpBtn");
 const addr = document.querySelector("#addr");
 const addrDet = document.querySelector("#addrDet");
+const checkId = document.querySelector(".checkId");
+const idChecked = document.querySelector("#idChecked");
+
 
 var re = /^[A-Za-z0-9]{5,15}[@]{1}[A-Za-z0-9]{5,10}[.]{1}[A-Za-z]{2,3}$/;
 
 signUpBtn.addEventListener("click", signUp);
 
+
 function signUp(e){
     e.preventDefault()
     if (!userid.value) {
         userid.focus();
-        return alert("아이디를 입력해주세요.");
+        // return alert("아이디를 입력해주세요.");
     }
     else if (!pwd.value) {
         pwd.focus();
-        return alert("비밀번호를 입력해주세요")
+        // return alert("비밀번호를 입력해주세요");
     }
     
     else if (pwd.value != checkPwd.value) {
         checkPwd.focus();
-        return alert("비밀번호가 일치하지 않습니다.");
+        // return alert("비밀번호가 일치하지 않습니다.");
     }
     else if (!username.value) {
         username.focus();
-        return alert("이름을 입력해주세요")
+        // return alert("이름을 입력해주세요")
     }
     else if (!email.value || re.test(email)) {
         email.focus();
-        return alert("올바른 이메일 주소를 입력하세요.");
+        // return alert("올바른 이메일 주소를 입력하세요.");
     }
     else if (!addr.value) {
         addr.focus();
-        return alert("주소");
+        // return alert("주소");
     }
     else if (!addrDet.value) {
         addrDet.focus();
-        return alert("상세 주소를 입력하세요.");
+        // return alert("상세 주소를 입력하세요.");
     }
     else if (userid.getAttribute("checkResult") == "fail"){
-        alert("아이디 중복체크를 해주시기 바랍니다.");
+        // alert("아이디 중복체크를 해주시기 바랍니다.");
         userid.focus();
         return false;
       }
@@ -54,5 +58,16 @@ function signUp(e){
 };
 
 function idOverlap() {
-    userid.addEventListener
-}
+    if(!userid.value) {
+        alert('아이디를 입력해주세요.')
+        return;
+    }
+    userid.addEventListener("keyup", () => {
+        userid.setAttribute("checkResult", "fail");
+        checkId.style.display = "inline";
+        idChecked.style.display = "none";
+    });
+    
+    
+};
+
