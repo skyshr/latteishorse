f2ddda68892dd7a1ffa121c4253c0b7e58ac9983
Sungjin <93691850+sungjinWi@users.noreply.github.com>
2021-12-07 14:39:03 +0900

    views copy

---
+++ views copy/js/login.js
@@ -0,0 +1,10 @@
+const userid = document.querySelector("#userid");
+const pwd = document.querySelector("#pwd");
+const logInBtn = document.querySelector("#logInBtn");
+
+logInBtn.addEventListener("click", logIn);
+
+function logIn(e) {
+    e.preventDefault();
+signUpBtn.addEventListener("click", signUp);
+};
