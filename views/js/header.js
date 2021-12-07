b6132c9d1d4424c731b1536ec8f1bf8a9de3e3ba
Sungjin <93691850+sungjinWi@users.noreply.github.com>
2021-12-07 17:35:24 +0900

    popup

--- views/js/header.js
+++ views/js/header.js
@@ -6,3 +6,16 @@ toggleBtn.addEventListener('click', () => {
     menu.classList.toggle('active');
     signUp.classList.toggle('active');
 });
+
+document.getElementById("login").addEventListener("click", function(){
+    var signUpWindow;
+    var popupX= (document.body.offsetWidth / 2) - (570 / 2);
+    var popupY= (window.screen.height / 2) - (350 / 2); 
+    signUpWindow = window.open("/login", "logInForm", "width=570, height=350, left = "+ popupX + ", top= "+ popupY + "resizable = no, scrollbars = no");
+});
+document.getElementById("signup").addEventListener("click", function(){
+    var logInWindow;
+    var popupX= (document.body.offsetWidth / 2) - (570 / 2);
+    var popupY= (window.screen.height / 2) - (350 / 2); 
+    logInWindow = window.open("/signup", "signupForm", "width=570, height=350, left = "+ popupX + ", top= "+ popupY + "resizable = no, scrollbars = no");
+});
